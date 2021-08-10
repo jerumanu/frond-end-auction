622006ca3b096c140787c4bea5041df134c29b24
jerumanu <emmanueljeru200@gmail.com>
2021-08-10 08:11:52 +0300

    add body tag and html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+ 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <html>
+    <body>
     <App />
+    </body>
+    </html>
   </React.StrictMode>,
   document.getElementById('root')
 );
