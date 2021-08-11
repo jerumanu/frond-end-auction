b4758c54bd9451318b97811203350c2dd09d6295
jerumanu <emmanueljeru200@gmail.com>
2021-08-11 13:30:08 +0300

    modified login

--- src/components/login.js
+++ src/components/login.js
@@ -1,37 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { login } from '../action/auth';
+import axios from 'axios';
 
-const login = () => {
+const Login = ({ login, isAuthenticated }) => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '' 
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        login(email, password);
+    };
+
+    const continueWithGoogle = async () => {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/google-oauth2/?redirect_uri=${process.env.REACT_APP_API_URL}/google`)
+
+            window.location.replace(res.data.authorization_url);
+        } catch (err) {
+
+        }
+    };
+
+    const continueWithFacebook = async () => {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/facebook/?redirect_uri=${process.env.REACT_APP_API_URL}/facebook`)
+
+            window.location.replace(res.data.authorization_url);
+        } catch (err) {
+
+        }
+    };
+
+    if (isAuthenticated) {
+        return <Redirect to='/' />
+    }
+
+
     return (
         <div className="auth-wrapper">
             <div className="auth-inner">
-            <form >
+            <form onSubmit={e => onSubmit(e)} >
                 <h3>Sign In</h3>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" 
+                    className="form-control"
+                     placeholder="Enter email"
+                     name='email'
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                     />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" 
+                    className="form-control" 
+                    placeholder="Enter password"
+                    name='password'
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength='6'
+                        required
+                         />
                 </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+               
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
             </form>
-            </div>
+           < button className='btn btn-danger mt-3' onClick={continueWithGoogle}>
+                Continue With Google
+            </button>
+            <br />
+            <button className='btn btn-primary mt-3' onClick={continueWithFacebook}>
+                Continue With Facebook
+            </button>
+            <p className='mt-3'>
+                Don't have an account? <Link to='/signup'>Sign Up</Link>
+            </p>
+            <p className='mt-3'>
+                Forgot your Password? <Link to='/reset-password'>Reset Password</Link>
+            </p>
         </div>
+            </div>
+        
     )
 }
 
-export default login
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth?.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
