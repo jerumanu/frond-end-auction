1a64655e2713d4f255db27912a8737323613b175
jerumanu <emmanueljeru200@gmail.com>
2021-08-11 13:27:53 +0300

    modified routers and imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
-import login from './components/login';
+import login from './components/login'; 
 import registration from './components/registration';
 import home from './components/home';
 import detail from './components/detail';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import store from './store';
 import { Provider } from 'react-redux';
-
-
+import Activate from './components/Activate';
+import Facebook from './components/Facebook';
+import Google from './components/Google';
+import ResetPassword from './components/ResetPassword';
+import ResetPasswordConfirm from './components/ResetPasswordConfirm';
 class App extends Component {
   render() {
   return (
@@ -43,6 +46,12 @@ class App extends Component {
             <Route path="/sign-in" component={login} />
             <Route path="/sign-up" component={registration} />
             <Route  path="/detail" component={detail} />
+            <Route exact path='/facebook' component={Facebook} />
+            <Route exact path='/google' component={Google} />
+            <Route exact path='/reset-password' component={ResetPassword} />
+            <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+            <Route exact path='/activate/:uid/:token' component={Activate} />
+       
           </Switch>
         {/* </div> */}
       {/* </div> */}
